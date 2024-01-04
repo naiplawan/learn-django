9e8f80f955b2136a477f9d3e6da50619916b4663
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2024-01-04 22:43:04 +0700

    feat:add code in views.py

--- chapter1-response/polls/views.py
+++ chapter1-response/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse 
+
+## write function to return request object to the browser
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
