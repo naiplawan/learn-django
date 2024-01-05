ba49da9400f27d7f28946d0defed9e3c5df7cf94
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2024-01-05 15:47:31 +0700

    feat:change view to generic view

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'polls' # This line is app-specific. It names the app so that Django can distinguish it from other apps that might have the same view or template names.
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
-# The next step is to point the root URLconf at the polls.urls module.
+#Change the index view in polls/views.py to use Django’s generic views system.
 
 
