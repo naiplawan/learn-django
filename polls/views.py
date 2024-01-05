de0f3d912042fdcf3c769c9ef11ee5801e86e1b3
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2024-01-05 13:46:53 +0700

    feat: add 404Code and render in views.py

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from .models import Question
+from django.http import Http404
 
 # Create your views here.
 
 ## write function to return request object to the browser
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s."% question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist") # raise 404 error if question does not exist
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
