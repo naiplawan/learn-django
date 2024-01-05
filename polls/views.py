7d34a885033c4fb9a5f39e1f278b0542addb891a
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2024-01-05 14:00:36 +0700

    feat:add get_object_or404

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse 
 from .models import Question
 from django.http import Http404
@@ -9,13 +9,13 @@ from django.http import Http404
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/index.html', context) # render function takes the request object as its first argument, a template name as its second argument and a dictionary as its optional third argument
 
 def detail(request, question_id):
     try:
-        question = Question.objects.get(pk=question_id)
+       question = get_object_or_404(Question, pk=question_id)
     except Question.DoesNotExist:
-        raise Http404("Question does not exist") # raise 404 error if question does not exist
+        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
